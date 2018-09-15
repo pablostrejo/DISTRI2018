146a1f2d48b26864836c8b852f5b2c9fafdf9127
Tomas <tmonteroswinnen@gmail.com>
2018-09-15 12:10:50 -0300

    Mazo

--- Servidor/src/negocio/Mazo.java
+++ Servidor/src/negocio/Mazo.java
@@ -1,5 +1,54 @@
 package negocio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mazo {
 
+	private int idMazo;
+	private List<Carta> cartas;
+	
+	public Mazo() {
+		super();
+		iniciarMazo();
+	}	
+	
+	public int getIdMazo() {
+		return idMazo;
+	}
+	public void setIdMazo(int idMazo) {
+		this.idMazo = idMazo;
+	}
+	public List<Carta> getCartas() {
+		return cartas;
+	}
+	public void setCartas(List<Carta> cartas) {
+		this.cartas = cartas;
+	}
+	
+	public void iniciarMazo(){
+		
+		
+		List<Carta> c = new ArrayList<Carta>(); 
+		this.cartas = c;
+		
+	}
+	public Carta obtenerCarta(){
+		
+		
+		return null;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
