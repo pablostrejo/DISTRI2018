1e4ef0c1ff0fe5895ad4ba082c4b3f4f1ff26580
Tomas <tmonteroswinnen@gmail.com>
2018-09-15 14:42:24 -0300

    Juego

--- Servidor/src/negocio/Juego.java
+++ Servidor/src/negocio/Juego.java
@@ -1,11 +1,57 @@
 package negocio;
 
+import java.util.List;
+
 public class Juego {
 	
 	private int idJuego;
 	private ModalidadEnum tipoJuego;
 	private List<Partida> partidas;
 	
+	public Juego(int idJuego, ModalidadEnum tipoJuego, List<Partida> partidas) {
+		super();
+		this.idJuego = idJuego;
+		this.tipoJuego = tipoJuego;
+		this.partidas = partidas;
+	}
+
+	public int getIdJuego() {
+		return idJuego;
+	}
+
+	public void setIdJuego(int idJuego) {
+		this.idJuego = idJuego;
+	}
+
+	public ModalidadEnum getTipoJuego() {
+		return tipoJuego;
+	}
+
+	public void setTipoJuego(ModalidadEnum tipoJuego) {
+		this.tipoJuego = tipoJuego;
+	}
+
+	public List<Partida> getPartidas() {
+		return partidas;
+	}
+
+	public void setPartidas(List<Partida> partidas) {
+		this.partidas = partidas;
+	}
+	
+	public void registrarMovimiento(Jugador jugador, Movimiento movimiento){
+		
+	}
+	public Movimiento obtenerMovimientoPosibles(Jugador jugador){
+		
+	}
+	
+	public boolean terminoJuego(){
+		
+		
+		return false;
+		
+	}
 	
 
 }
