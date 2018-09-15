3c67bda279c197f620a237d1f66dbca7b44335e3
tinchoo <tincho.varios@gmail.com>
2018-09-15 12:55:58 -0300

    Ranking & ItemRanking
    Las clases, atributos y firma de los métodos. Falta el desarrollo cuando esté listo el DAO.

---
+++ Servidor/src/negocio/ItemRanking.java
@@ -0,0 +1,28 @@
+package negocio;
+
+public class ItemRanking {
+	
+	private Jugador jugador;
+	private int puntos;
+	
+	public void actualizarPuntos() {
+		//TODO Para la instancia, debe recibir los nuevos puntos.
+	}
+
+	public Jugador getJugador() {
+		return jugador;
+	}
+
+	public void setJugador(Jugador jugador) {
+		this.jugador = jugador;
+	}
+
+	public int getPuntos() {
+		return puntos;
+	}
+
+	public void setPuntos(int puntos) {
+		this.puntos = puntos;
+	}
+
+}
---
+++ Servidor/src/negocio/Ranking.java
@@ -0,0 +1,40 @@
+package negocio;
+
+import java.util.List;
+
+public class Ranking {
+	
+	private List<ItemRanking> catNovato;
+	private List<ItemRanking> catCalificado;
+	private List<ItemRanking> catExperto;
+	private List<ItemRanking> catMaster;
+	
+	public void actualizarRanking(List<Jugador> jugadores) {
+		
+	}
+	
+	public List<ItemRanking> obtenerRankingNovato(){
+		//TODO cuando esté listo el DAO
+	}
+	
+	public List<ItemRanking> obtenerRankingNovatos(){
+		//TODO cuando esté listo el DAO	
+	}
+	
+	public List<ItemRanking> obtenerRankingCalificados(){
+		//TODO cuando esté listo el DAO
+	}
+	
+	public List<ItemRanking> obtenerRankingExpertos(){
+		//TODO cuando esté listo el DAO
+	}
+	
+	public List<ItemRanking> obtenerRankingMasters(){
+		//TODO cuando esté listo el DAO
+	}
+	
+	public List<ItemRanking> obtenerRankingTotal(){
+		//TODO cuando esté listo el DAO
+	}
+
+}
