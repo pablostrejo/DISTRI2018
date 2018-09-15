0201ba584f65fa40850bea2f94aa82c829be82ab
tinchoo <tincho.varios@gmail.com>
2018-09-15 13:36:10 -0300

    Grupo
    Está listo, aunque falta ver verificar cosas relacionadas con clases que aún no creamos. Ej Pareja.

---
+++ Servidor/src/negocio/Grupo.java
@@ -0,0 +1,37 @@
+package negocio;
+
+public class Grupo {
+	
+	private int idGrupo;
+	private String nombre;
+	private List<Jugador> miembros;
+	private List<Pareja> parejaEsperandoPareja;
+	private Jugador admin;
+	private List<Juego> juegos;
+	private Ranking ranking;
+	
+	public boolean hayJugadoresSuficientes() {
+		
+	}
+	
+	public boolean sosGrupo(String nombre) {
+		return (this.nombre==nombre);
+	}
+	
+	public void crearJuego(Pareja pareja1, Pareja pareja2) {
+		String modalidad = "librePareja";
+		Juego j = new Juego(pareja1, pareja2, modalidad ); //TODO aún no está el constructor de JUEGO, verificar la cantidad y el órden de los atributos.
+	}
+	
+	public Pareja crearPareja(Jugador Jugador1,Jugador Jugador2) {
+		Pareja p = new Pareja();
+		p.setJugador1(Jugador1);
+		p.setJugador2(Jugador2);
+		return p;
+	}
+	
+	public boolean sosAdmin(Jugador jugador) {
+		return (jugador.getIdJugador()==this.admin.getIdJugador());
+	}
+
+}
