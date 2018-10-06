89213e998c64e356f9fc44e41ad19153593dd2b1
tinchoo <tincho.varios@gmail.com>
2018-10-06 15:33:04 -0300

    BusinessDelegate
    Business delegate vacío

---
+++ Repositorio/src/delegado/BusinessDelegate.java
@@ -0,0 +1,25 @@
+package delegado;
+
+import java.net.MalformedURLException;
+
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import rmi.RemoteInterface;
+
+public class BusinessDelegate {
+
+	private RemoteInterface ri;
+	
+	
+	
+	public BusinessDelegate(){
+		
+	}
+	
+	
+	
+	
+	
+	
+}
